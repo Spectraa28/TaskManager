02a74a3f3f7920c7f4ef00dab298a92a57bb5397
Spectraa28 <sonuvermacollege@gmail.com>
2026-02-25 13:44:16 +0530

    chore: add global exception handling and app constants

---
+++ src/main/java/com/Project/TaskManager/config/AppConstants.java
@@ -0,0 +1,16 @@
+package com.Project.TaskManager.config;
+
+public class AppConstants {
+    
+        // API prefixes
+    public static final String API_PREFIX = "/api";
+    public static final String AUTH_PREFIX = "/api/auth";
+
+    // Pagination defaults
+    public static final int DEFAULT_PAGE = 0;
+    public static final int DEFAULT_SIZE = 20;
+    public static final String DEFAULT_SORT = "createdAt";
+    public static final String DEFAULT_SORT_DIR = "desc";
+
+    
+}
---
+++ src/main/java/com/Project/TaskManager/exceptions/UnauthorizedException.java
@@ -0,0 +1,8 @@
+package com.Project.TaskManager.exceptions;
+
+public class UnauthorizedException extends RuntimeException{
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+    
+}
